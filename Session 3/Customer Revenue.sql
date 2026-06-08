491b36ed59a961d6ee769a4d18519b1a9f194fbf
Aryan Ghanghas <165904242+codebuddy007@users.noreply.github.com>
2026-06-08 14:03:39 +0530

    Add SQL query for March 2019 customer revenue

---
+++ Session 3/Customer Revenue.sql
@@ -0,0 +1,12 @@
+SELECT 
+    cust_id, 
+    SUM(total_order_cost) AS revenue
+FROM 
+    orders
+WHERE 
+    EXTRACT(MONTH FROM order_date) = 3 
+    AND EXTRACT(YEAR FROM order_date) = 2019
+GROUP BY 
+    cust_id
+ORDER BY 
+    revenue DESC;
